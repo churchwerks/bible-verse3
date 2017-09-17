c7415f4025a7f67899ec0745b916653609e413ed
churchwerks <steven.lennis@icloud.com>
2017-09-16 21:40:38 -0500

    Stubbed out CLI working

--- lib/bible_verse/cli.rb
+++ lib/bible_verse/cli.rb
@@ -2,12 +2,38 @@ class BibleVerse::CLI
   def call
     puts "Topical Bible Verses"
     list_topics
+    menu
+    goodbye
   end
 
   def list_topics
-    puts <<-DOC
+    puts "Topical Bible Verses"
+    puts <<~DOC
       1. Peace Bible Verses - Bible Verses About Peace - Description
       2. Encouraging Bible Verses - Encouraging Bible Verses and Quotes - Description
     DOC
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the Topical Bible Verses you would like to search:
+      or type list to see more, or exit to quit."
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "Showing Bible Verses About Peace"
+      when "2"
+        puts "Showing Encouraging Bible Verses and Quotes"
+      when "list"
+        list_topics
+      else
+        puts "Not sure what you mean, type list or exit."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Peace be with you"
   end
 end
