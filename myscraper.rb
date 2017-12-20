b0a3b0fa97372b3384b4eb4bc4a4ccd8b9c9ff13
churchwerks <steven.lennis@icloud.com>
2017-12-20 13:38:30 -0600

    code before refactoring

--- myscraper.rb
+++ myscraper.rb
@@ -6,6 +6,6 @@ require 'open-uri'
 html = open("https://www.biblestudytools.com/topical-verses/")
 
 doc = Nokogiri::HTML(html)
-verses = doc.css(".xl-h3.list-group-item-heading")
+verses = doc.css(".list-group-item").first.css(".xl-h3.list-group-item-heading").text
 binding.pry
 puts verses
