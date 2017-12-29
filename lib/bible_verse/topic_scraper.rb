1be1e5923c5691bdac32d7beef26caa424ed54ba
churchwerks <steven.lennis@icloud.com>
2017-12-28 18:51:27 -0600

    code to delete

--- lib/bible_verse/topic_scraper.rb
+++ lib/bible_verse/topic_scraper.rb
@@ -1,4 +1,5 @@
 class BibleVerse::Tscraper
+  #
     def get_page
       Nokogiri::HTML(open("https://www.biblestudytools.com/topical-verses/"))
     end
@@ -9,6 +10,14 @@ class BibleVerse::Tscraper
 
     def make_topics
       get_topics.each do |t|
+        topic = self.new
+          topic.title = t.css(".xl-h3.list-group-item-heading").text
+          topic.description = t.css("p").text
+          topic.url = t.css("a").attribute("href").value
+          if topic.title != ""
+            topic.save
+          end
+        end
         BibleVerse::Topic.new_from_featured_topics(t)
       end
     end
